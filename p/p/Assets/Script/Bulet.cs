7f78adf45f623a2fc159df574478ebd90b6730a8
DESKTOP-M30FNO0\GA2B <h1202316001@hiratagakuen.onmicrosoft.com>
2024-11-22 17:06:20 +0900

    動画３途中、弾が進まない

---
+++ p/p/Assets/Script/Bulet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulet : MonoBehaviour
+{
+    [SerializeField,Header("’e–ò‚Ì‘¬“x")]
+    private float _speed;
+    [SerializeField, Header("’e‚ÌˆÐ—Í")]
+    private int _speer;
+
+    private Rigidbody2D _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void _Move()
+    {
+        _rigid.velocity = transform.up * _speed;
+    }
+}
--- p/p/Assets/Script/Player.cs
+++ p/p/Assets/Script/Player.cs
@@ -7,29 +7,44 @@ public class Player : MonoBehaviour
 {
     [SerializeField,Header("移動速度")]
     private float _speed;
+    [SerializeField, Header("弾オブジェクト")]
+    private GameObject _bullet;
+    [SerializeField, Header("弾を発射する時間")]
+    private float _shootTime;
 
-    private Vector2 _irputVeloclty;
+    private Vector2 _inputVelocity;
     private Rigidbody2D _rigid;
+    private float _shootCount;
 
 
     // Start is called before the first frame update    void Start()
     void Start()
     {
-        _irputVeloclty = Vector2.zero;
+        _inputVelocity = Vector2.zero;
         _rigid = GetComponent<Rigidbody2D>();
-    
+        _shootCount = 0;
     }
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
         _Move();
+        _Shooting();
     }
     private void _Move()
     {
-        _rigid.velocity = _irputVeloclty * _speed;
+        _rigid.velocity = _inputVelocity * _speed;
+    }
+    private void _Shooting()
+    {
+        _shootCount += Time.deltaTime;
+        if (_shootCount < _shootTime) return;
+
+        GameObject bulletObj = Instantiate(_bullet);
+        bulletObj.transform.position = transform.position + new Vector3(0f, transform.lossyScale.y/2.0f,0f);
+        _shootCount = 0.0f;
     }
     public void OnMove(InputAction.CallbackContext context)
     {
-        _irputVeloclty = context.ReadValue<Vector2>();        
+        _inputVelocity = context.ReadValue<Vector2>();        
     }
 }
