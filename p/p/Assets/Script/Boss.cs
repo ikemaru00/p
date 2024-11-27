6084006b7a44ab4e2a1183aa5fd4bf7f547b0f70
DESKTOP-CMFB67U\GA2B <h1202316001@hiratagakuen.onmicrosoft.com>
2024-11-27 12:38:37 +0900

    動画12終わり

--- p/p/Assets/Script/Boss.cs
+++ p/p/Assets/Script/Boss.cs
@@ -8,20 +8,40 @@ public class Boss : Enemy
     private float _limitPoyY;
     [SerializeField, Header("’ÊíUŒ‚‰ñ”")]
     private int _normalAttackCount;
+    [SerializeField, Header("î’e‚Ì’e”")]
+    private int _ougiBulleNum;
+    [SerializeField,Header("î’e‚ÌŠp“x")]
+    private float _ougiAngle;
+    [SerializeField, Header("î’e‚ÌUŒ‚‰ñ”")]
+    private int _ougiAttackCount;
+    [SerializeField, Header("ƒWƒOƒUƒO’eUŒ‚‚ÌŽžŠÔ")]
+    private float _LRAttackTime;
+    [SerializeField, Header("ƒWƒOƒUƒO’e‚ÌŠÔ‹÷")]
+    private float _LRShootTime;
+    [SerializeField, Header("ƒWƒOƒUƒO‚Ì•")]
+    private float _LRRange;
+    [SerializeField, Header("ƒWƒOƒUƒO‚Ì‘¬“x")]
+    private float _LRSpeed;
 
     enum AttackMode
     {
         Normal,
         Ougi,
+        LeftRight,
+        Circle,
     }
 
-    private int _currentNormalAttackCount;
+    private int _currentAttackCount;
     private AttackMode _attackMode;
+    private float _rotateZ;
+    private float _LRAttackCount;
 
     protected override void _Initialize()
     {
-        _currentNormalAttackCount = 0;
+        _currentAttackCount = 0;
         _attackMode = AttackMode.Normal;
+        _rotateZ = 0f;
+        _LRAttackCount = 0f;
     }
 
     protected override void _Move()
@@ -41,7 +61,9 @@ public class Boss : Enemy
        switch (_attackMode)
         {
             case AttackMode.Normal:_NormalShooting(); break;
-            case AttackMode.Ougi: break;
+            case AttackMode.Ougi: _OugiShooting(); break;
+            case AttackMode.LeftRight:_LeftRightShooting(); break;
+            case AttackMode.Circle:break;
         }
 
     }
@@ -57,13 +79,71 @@ public class Boss : Enemy
 
 
         _shootCount = 0f;
-        _currentNormalAttackCount++;
+        _currentAttackCount++;
 
-        if(_currentNormalAttackCount >= _normalAttackCount)
+        if(_currentAttackCount >= _normalAttackCount)
         {
             _attackMode = AttackMode.Ougi;
-            _currentNormalAttackCount = 0;
+            _currentAttackCount = 0;
         }
 
     }
+    private void _OugiShooting()
+    {
+        _shootCount += Time.deltaTime;
+        if (_shootCount < _shootTime) return;
+
+        for(int i=0; i < _ougiBulleNum;i++)
+        {
+            float angleRange = Mathf.Deg2Rad * _ougiAngle;
+            float theta = angleRange / (_ougiBulleNum - 1) * i - Mathf.Deg2Rad * (90f + _ougiAngle / 2f);
+            GameObject bullet = Instantiate(_bullet[1]);
+            bullet.transform.position = transform.position;
+            Vector3 dir = transform.position + new Vector3(Mathf.Cos(theta), Mathf.Sin(theta)) - transform.position;
+            bullet.transform.rotation = Quaternion.FromToRotation(transform.up, dir);
+        }
+
+        _shootCount = 0f;
+        _currentAttackCount++;
+
+        if (_currentAttackCount >= _ougiAttackCount)
+        {
+            _attackMode = AttackMode.LeftRight;
+            _currentAttackCount = 0;
+        }
+    }
+
+    private void _LeftRightShooting()
+    {
+        _LRAttackCount += Time.deltaTime;
+        if(_LRAttackCount >= _LRAttackTime)
+        {
+            _shootCount = 0f;
+            _LRAttackCount = 0;
+            _attackMode = AttackMode.Circle;
+        }
+
+
+        _shootCount += Time.deltaTime;
+        if (_shootCount < _LRShootTime) return;
+
+        _rotateZ += _LRSpeed;
+        if(_rotateZ > _LRRange)
+        {
+            _LRSpeed *= -1f;
+            _rotateZ = _LRRange;
+        }
+        else if(_rotateZ < -_LRRange)
+        {
+            _LRSpeed *= -1f;
+            _rotateZ = -_LRRange;
+        }
+
+        GameObject bullet = Instantiate(_bullet[2]);
+        bullet.transform.position = transform.position;
+        bullet.transform.eulerAngles = new Vector3(0f, 0f, -180f + _rotateZ);
+
+        _shootCount = 0f;
+    }
+
 }
